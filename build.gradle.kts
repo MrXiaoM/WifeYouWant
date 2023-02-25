dc52b78f9b80fc6b6f589dd2958548643969776d
MrXiaoM <coolxiaom95@gmail.com>
2023-02-25 08:44:30 +0800

    update 0.1.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.12.3"
+    id("net.mamoe.mirai-console") version "2.14.0"
 }
 
 group = "top.mrxiaom"
-version = "0.1.4"
+version = "0.1.5"
 
 repositories {
     maven("https://maven.aliyun.com/repository/central")
